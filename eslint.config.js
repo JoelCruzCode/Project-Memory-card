4558246e855040599bded3204b0a56debfea2d83
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-08-21 01:06:00 -0700

    install prettier, turn off prop-types rule

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,8 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-
+import prettier from 'eslint-plugin-prettier'
+// import prettierConfig from 'eslint-config-prettier'
 export default [
   { ignores: ['dist'] },
   {
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +35,7 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      "react/prop-types": "off",
     },
   },
 ]
