036e984cb9e6d758d98b577466227619b7bb3e0d
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-08-21 01:07:04 -0700

    feat: setup init state & logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+
+import { useEffect, useState } from 'react';
+import './App.css';
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import { getRandomPokemon, shufflePokemon, determinePokemonCount } from './pokemon';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [difficulty, setDifficulty] = useState('easy');
+  const [pokemon, setPokemon] = useState([]);
+  const [startGame, setStartGame] = useState(false)
+
+  // change name to loadGame? 
+  function initializeGame(e) {
+    const value = e.target.value
+    setDifficulty(value)
+
+    const pokemonCount = determinePokemonCount(value)
+
+    getRandomPokemon(pokemonCount).then((pokemonCards) => {
+      // console.log('Fetched Pokemon:', pokemonCards);
+      const shuffledPokemon = shufflePokemon(pokemonCards);
+      // console.log('shuffle', shuffledPokemon)
+      setPokemon(shuffledPokemon);
+  });
+
+  setStartGame(true)
+  }
+
+  useEffect(() => {
+    console.log('Pokemon State: ',pokemon)
+    console.log('difficulty', difficulty)
+  },[pokemon, difficulty])
 
   return (
     <>
-      
+      <StartScreen 
+        onChange={initializeGame} 
+        style={{display: startGame ? 'none': 'block'}}
+      />
+      {startGame ? <Game
+      pokemon={pokemon}
+      >
+      </Game> : null}
+      {/* Render the Pokémon cards or other components here */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
