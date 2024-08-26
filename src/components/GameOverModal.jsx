a3237e8aee71e1e1737288c289f67a01f1ba4606
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-08-25 21:56:05 -0700

    feat: add handler to let user play next level

--- src/components/GameOverModal.jsx
+++ src/components/GameOverModal.jsx
@@ -1,18 +1,32 @@
 
-    export default function GameOverModal ({difficulty, handleRestart, handleQuit}) {
+    export default function GameOverModal ({gameStatus, difficulty, handleRestart, handleNextLevel, handleQuit}) {
 
         return (
             <div 
             className="end-modal"
             // style={style}
-            >
+            > 
+            {gameStatus.won && 
+                         <label htmlFor="play-again">
+                         <input 
+                         type="radio"
+                         id="play-again"
+                         value={difficulty}
+                         onChange={(e)=> {
+                            handleNextLevel(e)
+                         }}
+                     
+                         />
+                         <span>Next Level?</span>
+                     </label>}
                 <h2>Game Over</h2>
                 <label htmlFor="restart">
                     <input 
                     type="radio"
                     id="restart"
                     value={difficulty}
-                    onChange={handleRestart}
+                    onChange={(e) => {
+                        handleRestart(e)}}
                 
                     />
                     <span>Play Again?</span>
