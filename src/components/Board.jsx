55ac5ed4e736a25364e19e47b872fb79a695ff15
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-08-25 21:54:22 -0700

    feat: pass state to board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,14 @@
 
-import Card from "./Card"
+import Card from "./Card";
 
-export default function Board ({pokemon}) {
-    return (
-        <div
-        className="board"
-        >
-            {pokemon.map(poke => (
-                <Card key={poke.id} pokemon={poke}></Card>
-                ))}
-        </div>
-    )
+export default function Board({ pokemon, gameStatus, onClick }) { 
+  return (
+    <div 
+    className="board"
+    >
+      {pokemon.map(poke => (
+        <Card key={poke.id} pokemon={poke} gameStatus={gameStatus} onClick={onClick} /> 
+      ))}
+    </div>
+  );
 }
