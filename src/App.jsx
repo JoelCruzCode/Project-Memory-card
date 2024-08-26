56bea5e0d5a96d687b3ce7742ac8118514d841d4
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-08-25 22:02:30 -0700

    feat: add handlers for game logic
    created all handlers to handle the various states and user interactions

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,162 @@
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import StartScreen from './components/StartScreen';
 import Game from './components/Game';
 import { getRandomPokemon, shufflePokemon, determinePokemonCount } from './pokemon';
-
+import GameOverModal from './components/GameOverModal';
+localStorage.clear()
 function App() {
   const [difficulty, setDifficulty] = useState('easy');
   const [pokemon, setPokemon] = useState([]);
-  const [startGame, setStartGame] = useState(false)
-
-  // change name to loadGame? 
-  function initializeGame(e) {
+  const [increment, setIncrement] = useState(0)
+  const [scores, setScores] = useState({
+    current: 0,
+    best: localStorage.getItem('best-score') || 0
+})
+  const [gameStatus, setGameStatus] = useState({
+    start: false,
+    end: false, 
+    won: false
+  })
+ 
+  // TODO might just change callback to useEffect and change "onChange aka initGame to update difficulty and status state"
+  // TODO alternatively i can use no hooks at all and just manage the increment change by using D-injection in initGame
+  const initializeGame = useCallback((e)=> {
     const value = e.target.value
+
     setDifficulty(value)
 
-    const pokemonCount = determinePokemonCount(value)
-
+    const pokemonCount = determinePokemonCount(value, increment)
+   
     getRandomPokemon(pokemonCount).then((pokemonCards) => {
-      // console.log('Fetched Pokemon:', pokemonCards);
-      const shuffledPokemon = shufflePokemon(pokemonCards);
-      // console.log('shuffle', shuffledPokemon)
+
+      const shuffledPokemon = shufflePokemon(pokemonCards, true);
+
       setPokemon(shuffledPokemon);
   });
 
-  setStartGame(true)
-  }
+  setGameStatus(game => ({...game, start: true, end: false}))
+  }, [increment])
+
+
+  useEffect(()=> {
+    // updates increment appropriately on next level
+    if(gameStatus.start) {
+      initializeGame({target: {value: difficulty}})
+    }
+    
+  }, [increment, difficulty, initializeGame, gameStatus.start])
+
 
   useEffect(() => {
     console.log('Pokemon State: ',pokemon)
     console.log('difficulty', difficulty)
-  },[pokemon, difficulty])
+    console.log('increment:' ,increment)
+  },[pokemon, difficulty, increment])
+
+
+  function handleClick(e) {
+    const id = e.currentTarget.dataset.id
+
+    const clickedPokemon = pokemon.find(poke => poke.id === id);
+    if(clickedPokemon.clicked) {
+
+      setGameStatus(game => ({ ...game, end: true})) 
+      return
+    }
+
+    incrementScore()
+    
+    const lastCard = pokemon.filter(poke => poke.clicked === false)
+
+    if(lastCard.length === 1) {
+      handleWinning()
+    }
+  
+
+    e.preventDefault()
+    setPokemon(prev => (
+      shufflePokemon(prev.map(pokemon => pokemon.id === id 
+        ? {...pokemon, clicked: true} 
+        : pokemon
+        )
+    )))
+  }
+
+
+  function handleRestart(e) {
+    setIncrement(0) // TODO might remove to let user continue at last card count 
+    setScores(score => ({
+      ...score, current: 0
+    }))
+    initializeGame(e)
+  }
+
+  
+
+  function handleNextLevel(e) {
+    console.log(e.target)
+    setIncrement(increment => increment + 3)
+    // initializeGame(e)
+  }
+
+
+  function handleQuit() {
+    setGameStatus(game => ({...game, start: false, end: false}))
+    setIncrement(0)
+  }
+
+  function incrementScore() {
+    const newScore = scores.current + 1
+    const newBestScore = Math.max(newScore, scores.best)
+
+    if(newBestScore > scores.best) {
+      localStorage.setItem('best-score', newBestScore)
+    }
+ 
+    setScores(score => ({
+      ...score, current: newScore, best: newBestScore
+    }))
+
+
+  }
+  
+  function handleWinning() {
+    setGameStatus(game => ({
+      ...game, end: true, won: true
+    }))
+  }
+  // TODO might have to download animation to handle flipping cards
+  // TODO make card only clickable once per animation
 
   return (
-    <>
+    <main className='main-background'>
       <StartScreen 
+        gameStatus={gameStatus}
         onChange={initializeGame} 
-        style={{display: startGame ? 'none': 'block'}}
+        style={{display: gameStatus.start ? 'none': 'block'}}
       />
-      {startGame ? <Game
+      {gameStatus.start ? <Game
+      gameStatus={gameStatus}
+      scores={scores}
       pokemon={pokemon}
+      onClick={handleClick}
+      
       >
       </Game> : null}
-      {/* Render the Pokémon cards or other components here */}
-    </>
+      {gameStatus.end 
+      ? <GameOverModal
+        gameStatus={gameStatus}
+        difficulty={difficulty}
+        handleRestart={handleRestart}
+        handleNextLevel={handleNextLevel}
+        handleQuit={handleQuit}
+        setIncrement={setIncrement}
+      /> 
+      : null}
+    </main>
+    
   );
 }
 
